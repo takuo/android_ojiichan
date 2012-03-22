7c4a7766914a563b2cb59ed9a73aadeec63dc340
koyachi <rtk2106@gmail.com>
2012-03-22 19:35:25 +0900

    update twitter4j-android-2.2.5

--- src/jp/takuo/android/ojiichan/Main.java
+++ src/jp/takuo/android/ojiichan/Main.java
@@ -3,7 +3,7 @@ package jp.takuo.android.ojiichan;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.http.AccessToken;
+import twitter4j.auth.AccessToken;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
